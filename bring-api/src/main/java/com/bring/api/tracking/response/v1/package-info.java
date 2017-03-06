a1b8501e9d4b1ac23cf4cdc68ca4b7eb094135e3
Manish Singh <manish.csingh@tcs.com>
2017-03-07 02:01:31 +0530

    move to v1 folder.

--- bring-api/src/main/java/com/bring/api/tracking/response/v1/package-info.java
+++ bring-api/src/main/java/com/bring/api/tracking/response/v1/package-info.java
@@ -1,2 +1,2 @@
 @javax.xml.bind.annotation.XmlSchema(namespace = "http://www.bring.no/sporing/1.0", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package com.bring.api.tracking.response;
+package com.bring.api.tracking.response.v1;
